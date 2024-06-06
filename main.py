d33e91d11fe1f3503a6169eaba133ad7a0cc9b01
Mohammed-Saleh-Ishaq <mohammedishaq0592@gmail.com>
2024-06-07 01:46:10 +0530

    added update/put method

--- main.py
+++ main.py
@@ -26,12 +26,25 @@ def read_tasks():
     return tasks
 
 # Path Parameters.
+# This will allow us to Read an individual task based on its ID.
 @app.get("/tasks/{task_id}", response_model= Task)
 def read_task(task_id: UUID):
     for task in tasks:
         if task.id == task_id:
             return task
-    return HTTPException(status_code = 404, detail = "Task not Found" )
+    raise HTTPException(status_code = 404, detail = "Task not Found" )
+
+# Update/Put method/ request
+@app.put("/tasks/{task_id}", response_model= Task)
+def update_task(task_id: UUID , task_update: Task):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            update_task = task.copy(update = task_update.dict(exclude_unset= True))
+            tasks[idx]=update_task
+            return update_task
+    raise HTTPException(status_code = 404, detail = "Task not Found" )
+
+
 
 if __name__ == "__main__":
     import uvicorn
