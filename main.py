dc6af27ecf263d26f1473f10e062c056ce0c769b
Mohammed-Saleh-Ishaq <mohammedishaq0592@gmail.com>
2024-06-06 23:58:47 +0530

    added path parameter/get method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from uuid import UUID , uuid4
@@ -13,17 +13,25 @@ class Task(BaseModel):
     completed : bool = False
 
 tasks = []
-
-@app.post("/tasks/", response_model= Task)
+# Create method task .
+@app.post("/tasks/", response_model= Task)  
 def create_task(task : Task):
     task.id = uuid4()
     tasks.append(task)
     return task
 
-@app.get("/tasks/", response_model=List[Task])
+# Get/Read method .
+@app.get("/tasks/", response_model=List[Task])  
 def read_tasks():
     return tasks
 
+# Path Parameters.
+@app.get("/tasks/{task_id}", response_model= Task)
+def read_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    return HTTPException(status_code = 404, detail = "Task not Found" )
 
 if __name__ == "__main__":
     import uvicorn
